060daea9e5a36f4df873daf4467bce5f5fb34e2e
David Agu <david.agu@interswitchng.com>
2025-04-02 16:50:58 +0100

    update: add player and ad main to scene

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import * as THREE from 'three';
 import { Renderer } from './components/renderer';
 import { Camera } from './components/camera';
-// import { Player } from './components/Player';
+import { player } from './components/player';
 
 import './style.css'
 
 const scene = new THREE.Scene();
+scene.add(player)
+
+// ambient light
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
+//directional light 
+const directionalLight = new THREE.DirectionalLight();
+directionalLight.position.set(-100, -100, 200);
+scene.add(directionalLight);
+
 
 const camera = Camera();
 scene.add(camera)
