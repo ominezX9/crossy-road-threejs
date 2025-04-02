d389c9861949b3614f48f4decb2b78acd7f5a555
David Agu <david.agu@interswitchng.com>
2025-04-02 16:13:07 +0100

    update: setup camera main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { Renderer } from './components/renderer';
-// import { Camera } from './components/Camera';
+import { Camera } from './components/camera';
 // import { Player } from './components/Player';
 
 import './style.css'
@@ -8,6 +8,7 @@ import './style.css'
 const scene = new THREE.Scene();
 
 const camera = Camera();
+scene.add(camera)
 
 const renderer = Renderer();
 renderer.render(scene, camera);
