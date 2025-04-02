06f5d0152c329d4061e189d64a5543f36a926bd5
David Agu <david.agu@interswitchng.com>
2025-04-02 16:57:14 +0100

    feat: types set up

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+import * as THREE from 'three';
+
+export type RowType = "forest" | "car" | "truck";
+
+export type Row =
+    | {
+        type: "forest";
+        trees: { titleIndex: number; height: number }[];
+    }
+    | {
+        type: "car";
+        direction: boolean;
+        speed: number;
+        vehicles: {
+            initialTileIndex: number;
+            color: THREE.ColorRepresentation;
+            ref?: THREE.Object3D;
+        }[];
+    }
+    | {
+        type: "truck";
+        direction: boolean;
+        speed: number;
+        vehicles: {
+            initialTileIndex: number;
+            color: THREE.ColorRepresentation;
+            ref?: THREE.Object3D;
+        }[];
+    };
