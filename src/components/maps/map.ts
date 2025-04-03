ae012dedbbf37d9d44f75dbb6c4357a63aa0fc50
David Agu <david.agu@interswitchng.com>
2025-04-03 12:19:00 +0100

    update: template for forest

--- src/components/maps/map.ts
+++ src/components/maps/map.ts
@@ -1,9 +1,22 @@
 import * as THREE from "three";
 import { Grass } from "../shared/grass";
+import { Row } from "../../types";
+
+
+export const metadata: Row[] = [
+    {
+        type: "forest",
+        trees: [
+            { titleIndex: -3, height: 50 },
+            { titleIndex: 2, height: 20 },
+            { titleIndex: 5, height: 50 },
+        ]
+    }
+]
 
 export const map = new THREE.Group();
 
 export function initializeMap() {
-    const grass = Grass(2);
+    const grass = Grass(0);
     map.add(grass)
 }
