b401d438ec3e5e458073c41f62349699a15dd594
David Agu <david.agu@interswitchng.com>
2025-04-03 13:01:32 +0100

    update: render trees to environment (scene)

--- src/components/maps/map.ts
+++ src/components/maps/map.ts
@@ -1,15 +1,16 @@
 import * as THREE from "three";
 import { Grass } from "../shared/grass";
 import { Row } from "../../types";
+import { Tree } from "../shared/tree";
 
 
 export const metadata: Row[] = [
     {
         type: "forest",
         trees: [
-            { titleIndex: -3, height: 50 },
-            { titleIndex: 2, height: 20 },
-            { titleIndex: 5, height: 50 },
+            { tileIndex: -3, height: 50 },
+            { tileIndex: 2, height: 20 },
+            { tileIndex: 5, height: 50 },
         ]
     }
 ]
@@ -18,5 +19,23 @@ export const map = new THREE.Group();
 
 export function initializeMap() {
     const grass = Grass(0);
-    map.add(grass)
+    map.add(grass);
+    addRows();
+}
+
+export function addRows() {
+    metadata.forEach((rowData, index) => {
+        const rowIndex = index + 1;
+
+        if (rowData.type === 'forest') {
+            const row = Grass(rowIndex);
+
+            rowData.trees.forEach(({tileIndex, height}) => {
+                const three = Tree(tileIndex, height);
+                row.add(three)
+            });
+
+            map.add(row)
+        }
+    })
 }
